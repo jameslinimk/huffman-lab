84def8fed253386e21fe27b8fa8ddb94babecf4e
Jameslinimk <jameskimimk@gmail.com>
2024-02-25 22:47:11 -0500

    qwe

--- main.ts
+++ main.ts
@@ -10,6 +10,13 @@ const frequencies = (input: string): Frequencies => {
     return result
 }
 
+/**
+ * For every character in b that is not in a, add it to a with a value of 0
+ */
+const populateFq = (a: Frequencies, b: Frequencies) => {
+    for (const char of Object.keys(b)) if (!a[char]) a[char] = 0
+}
+
 interface Character {
     character: string
     value: number
@@ -61,17 +68,16 @@ class BTree {
 
 type Item = BTree | Character
 
-const isBTree = (node: Item): node is BTree => (node as BTree).btree === true
+const buildTree = (q: Tinyqueue<Item>): [Record<string, string>, BTree] => {
+    const isBTree = (node: Item): node is BTree => (node as BTree).btree === true
+    const charToBTree = (char: Character): BTree => new BTree(char.value, char.character, null, null)
+    const combine = (a: Item, b: Item): BTree => {
+        const value = a.value + b.value
+        if (!isBTree(a)) a = charToBTree(a)
+        if (!isBTree(b)) b = charToBTree(b)
+        return new BTree(value, "", a as BTree, b as BTree)
+    }
 
-const charToBTree = (char: Character): BTree => new BTree(char.value, char.character, null, null)
-const combine = (a: Item, b: Item): BTree => {
-    const value = a.value + b.value
-    if (!isBTree(a)) a = charToBTree(a)
-    if (!isBTree(b)) b = charToBTree(b)
-    return new BTree(value, "", a as BTree, b as BTree)
-}
-
-const createCodes = (q: Tinyqueue<Item>): [Record<string, string>, BTree] => {
     let current = q.pop()
     while (current) {
         let deck = q.pop()
@@ -103,8 +109,11 @@ const createCodes = (q: Tinyqueue<Item>): [Record<string, string>, BTree] => {
     return [codes, base]
 }
 
-const bitLength = (fq: Frequencies, codes: Record<string, string>) =>
-    Object.entries(fq).reduce((acc, [char, value]) => acc + value * codes[char].length, 0)
+const huffmanLength = (fq: Frequencies, codes: Record<string, string>) =>
+    Object.entries(fq).reduce((acc, [char, value]) => {
+        if (!codes[char]) throw new Error(`No code for ${char.codePointAt(0)}`)
+        return acc + value * codes[char].length
+    }, 0)
 
 const encode = (input: string, codes: Record<string, string>): string => {
     let output = ""
@@ -125,13 +134,6 @@ const decode = (encoded: string, tree: BTree): string => {
     return decoded
 }
 
-const timeFunc = <T>(f: () => T, message: string) => {
-    const start = performance.now()
-    const value = f()
-    console.log(chalk.blue(`${(performance.now() - start).toFixed(2)}ms to ${message}`))
-    return value
-}
-
 const fixedBitEncoding = (frequencies: Record<string, number>) => {
     const characters = Object.keys(frequencies).length
     return Math.ceil(Math.log2(characters))
@@ -139,7 +141,7 @@ const fixedBitEncoding = (frequencies: Record<string, number>) => {
 
 const SEP = "       "
 const getAverageFrequencies = async (): Promise<Frequencies> => {
-    const text = await readFile("frequencies.txt", "utf-8")
+    const text = await readFile("./data/frequencies.txt", "utf-8")
     return Object.fromEntries(
         text
             .split("\n")
@@ -151,107 +153,102 @@ const getAverageFrequencies = async (): Promise<Frequencies> => {
     )
 }
 
-const avgBitLength = (codes: Record<string, string>) => {
-    const characters = Object.keys(codes).length
-    return Object.values(codes).reduce((acc, code) => acc + code.length, 0) / characters
+const timeFunc = <T>(f: () => T, message: string) => {
+    const start = performance.now()
+    const value = f()
+    console.log(chalk.blue(`${(performance.now() - start).toFixed(2)}ms to ${message}`))
+    return value
 }
 
-const main = async () => {
-    const input = await readFile("moby-dick.txt", "utf-8")
-    const fq = frequencies(input)
-
-    const pq = new Tinyqueue<Item>(undefined, (a, b) => a.value - b.value)
-    for (const [character, value] of Object.entries(fq)) pq.push({ character, value })
-
-    const [codes, base] = timeFunc(() => createCodes(pq), "create codes")
-
-    await writeFile("./output.json", JSON.stringify({ codes, base }, null, 4))
-    await writeFile("./output-tree.txt", base.visualize(codes))
+const output = async (codes: Record<string, string>, base: BTree, mod = "") => {
+    mod = mod ? `-${mod}` : ""
+    await writeFile(`./out/output${mod}.json`, JSON.stringify({ codes, base }, null, 4))
+    await writeFile(`./out/output-tree${mod}.txt`, base.visualize(codes))
     console.log(chalk.gray("> Outputted to ./output.json & ./output-tree.txt"))
     console.log(chalk.gray(`> Tree size: ${Object.keys(codes).length}`))
+}
 
-    const encoded = timeFunc(() => encode(input, codes), "encode")
-    const decoded = timeFunc(() => decode(encoded, base), "decode")
-
+const verify = (input: string, codes: Record<string, string>, base: BTree) => {
+    const encoded = encode(input, codes)
+    const decoded = decode(encoded, base)
     if (input === decoded) {
         console.log(chalk.green("Verification successful"))
     } else {
         console.log(chalk.red("Verification failed"))
     }
+}
 
-    const huffmanSize = bitLength(fq, codes)
-    const fixedSize = input.length * fixedBitEncoding(fq)
+const pqFromFrequencies = (fq: Frequencies) => {
+    const pq = new Tinyqueue<Item>(undefined, (a, b) => a.value - b.value)
+    for (const [character, value] of Object.entries(fq)) pq.push({ character, value })
+    return pq
+}
+
+/**
+ * How much smaller is a from b
+ */
+const percentDiff = (a: number, b: number) => (((a - b) / a) * 100).toFixed(2)
+
+const main = async () => {
+    const mobyDick = await readFile("./data/moby.txt", "utf-8")
+    const fq = frequencies(mobyDick)
+
+    const pq = pqFromFrequencies(fq)
+    const [codes, base] = timeFunc(() => buildTree(pq), "create codes")
+
+    await output(codes, base)
+    verify(mobyDick, codes, base)
+
+    const huffmanSize = huffmanLength(fq, codes)
+    const fixedSize = mobyDick.length * fixedBitEncoding(fq)
 
     console.log(chalk.yellow(`${huffmanSize.toLocaleString("en-US")} bits for huffman encoding`))
-    console.log(chalk.yellow(`${fixedSize.toLocaleString("en-US")} bits for huffman encoding`))
-    console.log(chalk.yellow(`${(((fixedSize - huffmanSize) / fixedSize) * 100).toFixed(2)}% space saved`))
+    console.log(chalk.yellow(`${fixedSize.toLocaleString("en-US")} bits for fixed encoding`))
+    console.log(chalk.yellow(`${percentDiff(fixedSize, huffmanSize)}% space saved`))
 
     // Challenge #1
-
     console.log(chalk.redBright("\nChallenge #1\n"))
 
-    const averages = await getAverageFrequencies()
-    Object.keys(fq).forEach((char) => {
-        if (!averages[char]) averages[char] = 0
-    })
+    const averageFq = await getAverageFrequencies()
+    populateFq(averageFq, fq)
 
-    const newPq = new Tinyqueue<Item>(undefined, (a, b) => a.value - b.value)
-    for (const [character, value] of Object.entries(averages)) newPq.push({ character, value })
+    const newPq = pqFromFrequencies(averageFq)
+    const [avgCodes, avgBase] = timeFunc(() => buildTree(newPq), "create average codes")
 
-    const [avgCodes, avgBase] = timeFunc(() => createCodes(newPq), "create average codes")
+    await output(avgCodes, avgBase, "avg")
+    verify(mobyDick, avgCodes, avgBase)
 
-    await writeFile("./output-avg.json", JSON.stringify({ codes: avgCodes, base: avgBase }, null, 4))
-    await writeFile("./output-avg-tree.txt", avgBase.visualize(avgCodes))
-    console.log(chalk.gray("> Outputted to ./output-avg.json & ./output-avg-tree.txt"))
-    console.log(chalk.gray(`> Tree size: ${Object.keys(avgCodes).length}`))
+    const avgSize = huffmanLength(fq, avgCodes)
 
-    const avgEncoded = timeFunc(() => encode(input, avgCodes), "encode average")
-    const avgDecoded = timeFunc(() => decode(avgEncoded, avgBase), "decode average")
-
-    if (input === avgDecoded) {
-        console.log(chalk.green("Verification successful"))
-    } else {
-        console.log(chalk.red("Verification failed"))
-    }
-
-    const avgSize = bitLength(fq, avgCodes)
     console.log(chalk.yellow(`${avgSize.toLocaleString("en-US")} bits for average huffman encoding`))
-    console.log(chalk.yellow(`${(((fixedSize - avgSize) / fixedSize) * 100).toFixed(2)}% space saved`))
-    console.log(chalk.yellow(`Huffman ${(((avgSize - huffmanSize) / avgSize) * 100).toFixed(2)}% better`))
+    console.log(chalk.yellow(`${percentDiff(fixedSize, avgSize)}% space saved`))
+    console.log(chalk.yellow(`Huffman ${percentDiff(avgSize, huffmanSize)}% better`))
 
     // Challenge #2
-
     console.log(chalk.redBright("\nChallenge #2\n"))
 
-    const otherBook = await readFile("other.txt", "utf-8")
+    const otherBook = await readFile("./data/other.txt", "utf-8")
     const otherFq = frequencies(otherBook)
 
-    const otherPq = new Tinyqueue<Item>(undefined, (a, b) => a.value - b.value)
-    for (const [character, value] of Object.entries(otherFq)) otherPq.push({ character, value })
+    // Since this book has chars not in avg or moby, we need to re-calculate the average and huffman codes
+    populateFq(fq, otherFq)
+    populateFq(averageFq, otherFq)
 
-    const [otherCodes, otherBase] = timeFunc(() => createCodes(otherPq), "create other codes")
+    const [newCodes] = timeFunc(() => buildTree(pqFromFrequencies(fq)), "create new codes")
+    const [newAvgCodes] = timeFunc(() => buildTree(pqFromFrequencies(averageFq)), "create new average codes")
 
-    await writeFile("./output-other.json", JSON.stringify({ codes: otherCodes, base: otherBase }, null, 4))
-    await writeFile("./output-other-tree.txt", otherBase.visualize(otherCodes))
-    console.log(chalk.gray("> Outputted to ./output-other.json & ./output-other-tree.txt"))
-    console.log(chalk.gray(`> Tree size: ${Object.keys(otherCodes).length}`))
+    const otherAvgSize = huffmanLength(otherFq, newAvgCodes)
+    const otherHuffmanSize = huffmanLength(otherFq, newCodes)
+    const otherFixedSize = otherBook.length * fixedBitEncoding(otherFq)
 
-    const otherEncoded = timeFunc(() => encode(otherBook, otherCodes), "encode other")
-    const otherDecoded = timeFunc(() => decode(otherEncoded, otherBase), "decode other")
-
-    if (otherBook === otherDecoded) {
-        console.log(chalk.green("Verification successful"))
-    } else {
-        console.log(chalk.red("Verification failed"))
-    }
-
-    const otherSize = bitLength(otherFq, otherCodes)
-    // const otherUsingHuffman = bitLength(otherFq, codes)
-    // const otherUsingAvg = bitLength(otherFq, avgCodes)
-    // const otherFixed = otherBook.length * fixedBitEncoding(otherFq)
-
-    // console.log(chalk.yellow(`${otherSize.toLocaleString("en-US")} bits for other huffman encoding`))
-    // console.log(chalk.yellow(`Huffman ${(((otherSize - otherUsingHuffman) / otherSize) * 100).toFixed(2)}% better`))
-    // console.log(chalk.yellow(`Average ${(((otherSize - otherUsingAvg) / otherSize) * 100).toFixed(2)}% better`))
+    console.log(chalk.yellow(`${otherAvgSize.toLocaleString("en-US")} bits for average huffman encoding`))
+    console.log(chalk.yellow(`${otherHuffmanSize.toLocaleString("en-US")} bits for huffman encoding`))
+    console.log(chalk.yellow(`Huffman is ${percentDiff(otherAvgSize, otherHuffmanSize)}% better than avg`))
+    console.log(chalk.yellow(`${otherFixedSize.toLocaleString("en-US")} bits for fixed encoding`))
+    console.log(
+        chalk.yellow(
+            `NewCodes Length: ${Object.keys(newCodes).length} | NewAvgCodes Length: ${Object.keys(newAvgCodes).length}`
+        )
+    )
 }
 main()
